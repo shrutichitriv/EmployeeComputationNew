0e90019a79851973dbd1770c9af76047b64285c0
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-11 16:37:40 +0530

    [Shruti] Calculate to save the total wage for each company

--- EmpWage2_244/EmpWage2_244/Program.cs
+++ EmpWage2_244/EmpWage2_244/Program.cs
@@ -21,9 +21,17 @@
 
             //RefactorCode.ComputeEmpWage();
 
-            MultipleCompany.ComputeEmpWage("TCS", 22, 25, 20);
-            MultipleCompany.ComputeEmpWage("Infosys", 15, 26, 24);
+            //MultipleCompany.ComputeEmpWage("TCS", 22, 25, 20);
+            //MultipleCompany.ComputeEmpWage("Infosys", 15, 26, 24);
+
             
+            TotalWageEachCompany Amazon = new TotalWageEachCompany("Amazon", 30, 25, 20);
+            TotalWageEachCompany Infosys = new TotalWageEachCompany("Infosys", 25, 22, 29);
+            Amazon.CompanyEmpWage();
+            Console.WriteLine(Amazon.toString());
+            Infosys.CompanyEmpWage();
+            Console.Write(Infosys.toString());
+
 
 
 
---
+++ EmpWage2_244/EmpWage2_244/TotalWageEachCompany.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage2_244
+{
+    public class TotalWageEachCompany
+    {
+
+        //Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private string Company;
+        private int EMP_RATE_PER_HR;
+        private int NUM_OF_WORKING_DAYS;
+        private int MAX_HR_IN_MONTH;
+        private int totalEmpWage;
+        public TotalWageEachCompany(string Company, int EMP_RATE_PER_HR, int NUM_OF_WORKING_DAYS, int MAX_HR_IN_MONTH)
+        {
+            this.Company = Company;
+            this.EMP_RATE_PER_HR = EMP_RATE_PER_HR;
+            this.NUM_OF_WORKING_DAYS = NUM_OF_WORKING_DAYS;
+            this.MAX_HR_IN_MONTH = MAX_HR_IN_MONTH;
+        }
+        public void CompanyEmpWage()
+        {
+            //Variable
+            int empHr = 0;
+            int totalWorkingDays = 0;
+            int totalEmpHr = 0;
+
+            while (totalEmpHr <= MAX_HR_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                //computation
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHr = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHr = 8;
+                        break;
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                totalEmpHr = totalEmpHr + empHr;
+                Console.WriteLine("Day: " + totalWorkingDays + " Employee Hour is: " + empHr);
+            }
+            totalEmpWage = totalEmpHr * EMP_RATE_PER_HR;
+            Console.WriteLine("Total Employee wage for Company: " + Company + " is: " + totalEmpWage);
+        }
+        public string toString()
+        {
+            return " Total Employee wage for Company: " + this.Company + " is: " + this.totalEmpWage;
+        }
+    }
+}
